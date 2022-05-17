04babf7527b3e36c83e3d6f0d14f2ad97f0b8215
Vilius <vilius.sreiberis@gmail.com>
2022-05-17 14:30:18 +0300

    firstCommit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import './App.scss';
+import FileUpload from './FileUpload/FileUpload';
+import FileList from './FileList/FileList';
+
+function App() {
+  const [files, setFiles] = useState([{
+    name: 'initlaFile'
+  }, {
+    name: 'secondFile',
+    isUploading: true,
+  }])
+
+  const removeFile = (filename) => {
+    setFiles(files.filter(file => file.name !== filename))
+  }
+
+  return (
+    <div className="App">
+      <div className="title">Upload file</div>
+      <FileUpload files={files} setFiles={setFiles}
+        removeFile={removeFile} />
+      <FileList files={files} removeFile={removeFile} />
+    </div>
+  );
+}
+
+export default App;
